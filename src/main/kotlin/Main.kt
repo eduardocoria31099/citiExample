3e69f7bd75a1b56dba59fef81d1e7430e00bb872
eduardocoria31099 <eduardo.coria@altatec.com.mx>
2023-06-03 19:08:13 -0600

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,95 @@
+import Dummies.productos
+import Dummies.productosFalse
+
+private var listSize: Int = 0
+private lateinit var successList: MutableList<Productos>
+private lateinit var errorList: MutableList<Productos>
+
+private lateinit var baseList: List<Productos>
+
+fun main() {
+    init()
+    getLista()
+}
+
+private fun init() {
+    errorList = mutableListOf()
+    successList = mutableListOf()
+    listSize = productos.size
+}
+
+private fun getLista() {
+    println("LOADING ...")
+
+    if (listSize > 5) {
+        println("mayor 5")
+        println(listSize)
+        val listSubFirst = productos.subList(fromIndex = 0, toIndex = 5)
+        val listSubLast = productos.subList(fromIndex = 5, toIndex = listSize)
+
+        println(listSubFirst.size)
+        println(listSubLast.size)
+        println(listSubFirst)
+        println(listSubLast)
+
+        firstCallService(listSubFirst)
+        firstCallService(listSubLast)
+        selectList()
+        validateList()
+    } else {
+        println("5 o menos")
+        firstCallService(productos)
+        selectList()
+        validateList()
+    }
+}
+
+private fun firstCallService(listProducts: List<Productos>) {
+    println("Llamado al servicio 1")
+    baseList = listProducts
+    println(baseList.size)
+
+}
+
+private fun secondCallService(listProducts: List<Productos>) {
+    println("Llamado al servicio 2")
+    listProducts.forEachIndexed { index, productos ->
+        if (productos.responseFlag == true) {
+            successList.add(productos)
+            errorList.removeAt(index)
+        }
+    }
+    println("success List ${successList.size}")
+    println("error List ${errorList.size}")
+
+    if (successList.size == listSize) {
+        println("Operacion exitosa 2")
+    } else {
+        if (errorList.size == listSize) {
+            println("Operacion erronea")
+        } else {
+            println("No se pudieron diferir algunas compras")
+        }
+    }
+}
+
+private fun validateList() {
+    if (successList.size == listSize) {
+        println("Operacion exitosa 1")
+    } else {
+        secondCallService(productosFalse)
+    }
+}
+
+private fun selectList() {
+    baseList.forEach {
+        if (it.responseFlag == true) {
+            successList.add(it)
+        } else {
+            errorList.add(it)
+        }
+    }
+
+    println("success List ${successList.size}")
+    println("error List ${errorList.size}")
+}
---
+++ src/main/kotlin/Productos.kt
@@ -0,0 +1,12 @@
+data class Productos(
+    val loanTenure: Int?,
+    val deferredAmount: Double?,
+    val campaignId: String?,
+    val waveId: String?,
+    val offerId: String?,
+    val installmentAmount: Double?,
+    val interestAmount: Double?,
+    val interestRate: Double?,
+    val principalPaidAmount: Double?,
+    val responseFlag: Boolean?,
+)
